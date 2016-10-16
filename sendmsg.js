e3ada2d019887884367cedb8f600f43d8a1de5d3
System Administrator <--global>
2016-10-16 13:51:07 -0500

    put 5000 messages

--- sendmsg.js
+++ sendmsg.js
@@ -18,7 +18,9 @@ var params = {
   MessageBody: JSON.stringify(body),
   QueueUrl: "https://sqs.us-west-2.amazonaws.com/942635221058/smarttools"
 };
-sqs.sendMessage(params, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else console.log(data);           // successful response
-});
+for (var i = 0; i < 1; i++) {
+  sqs.sendMessage(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);           // successful response
+  });
+}
