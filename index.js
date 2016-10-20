efed418b61c71d62d01c4494d13017e2a3946a12
System Administrator <--global>
2016-10-19 22:11:29 -0500

    fix routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var cron = require('node-cron');
 var aws = require('aws-sdk');
 var _ = require('underscore');
 var ffmpeg = require('fluent-ffmpeg');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var fs = require('fs');
 
 //Database credentials
@@ -10,24 +10,24 @@ var databaseConf = JSON.parse(fs.readFileSync('relational-conf.json', 'utf8'));
 
 //DataBase connection
 var connection = mysql.createConnection({
-  host     : databaseConf.host,
-  user     : databaseConf.user,
-  password : databaseConf.password,
-  database : databaseConf.database,
-  port     : 3306,
+  host: databaseConf.host,
+  user: databaseConf.user,
+  password: databaseConf.password,
+  database: databaseConf.database,
+  port: 3306,
   timeout: 60000
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
- 
+
   console.log('connected as id ' + connection.threadId);
 });
 
-var sender   = "da.prieto1@uniandes.edu.co";
+var sender = "da.prieto1@uniandes.edu.co";
 var verifiedEmails = [];
 
 // Load your AWS credentials and try to instantiate the object.
@@ -37,116 +37,117 @@ aws.config.loadFromPath(__dirname + '/aws-config.json');
 var ses = new aws.SES();
 
 var verify = function (email) {
-	var params = {
-        EmailAddress: email
-    };
-    
-    ses.verifyEmailAddress(params, function(err, data) {
-        if(err) {
-        	console.log('ERROR VERIFYING MAIL: ' + err);
-    	} 
-    	else {
-        	console.log('SUCCESS VERIFYING MAIL');
-    	} 
-    });
+  var params = {
+    EmailAddress: email
+  };
+
+  ses.verifyEmailAddress(params, function (err, data) {
+    if (err) {
+      console.log('ERROR VERIFYING MAIL: ' + err);
+    }
+    else {
+      console.log('SUCCESS VERIFYING MAIL');
+    }
+  });
 }
 
 var sendMail = function (email, constestId) {
-	
-	var ses_mail = "From: 'AWS Tutorial Series' <" + sender + ">\n";
-	ses_mail = ses_mail + "To: " + email + "\n";
-	ses_mail = ses_mail + "Subject: Video Successfully converted\n";
-    ses_mail = ses_mail + "--NextPart\n";
-    ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
-    ses_mail = ses_mail + "<b>Tu video ha sido publicado porfavor visitanos en <a>http://localhost:3000/smarttools/" + constestId + "</a></b>\n\n";
-    ses_mail = ses_mail + "--NextPart\n";
 
-	var params = {
-    	RawMessage: { Data: new Buffer(ses_mail) },
-    	Destinations: [ email ],
-    	Source: "'SmartTools Team' <" + email + ">'"
-	};
+  var ses_mail = "From: 'AWS Tutorial Series' <" + sender + ">\n";
+  ses_mail = ses_mail + "To: " + email + "\n";
+  ses_mail = ses_mail + "Subject: Video Successfully converted\n";
+  ses_mail = ses_mail + "--NextPart\n";
+  ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+  ses_mail = ses_mail + "<b>Tu video ha sido publicado porfavor visitanos en <a>http://localhost:3000/smarttools/" + constestId + "</a></b>\n\n";
+  ses_mail = ses_mail + "--NextPart\n";
 
-	ses.sendRawEmail(params, function(err, data) {
-    	if(err) {
-        	console.log('ERROR SENDING MAIL: ' + err);
-    	} 
-    	else {
-        	console.log('SUCCESS SENDING MAIL');
-    	}           
-	});		
+  var params = {
+    RawMessage: { Data: new Buffer(ses_mail) },
+    Destinations: [email],
+    Source: "'SmartTools Team' <" + email + ">'"
+  };
+
+  ses.sendRawEmail(params, function (err, data) {
+    if (err) {
+      console.log('ERROR SENDING MAIL: ' + err);
+    }
+    else {
+      console.log('SUCCESS SENDING MAIL');
+    }
+  });
 }
 
-var list = function() {
-	verifiedEmails = [];
-	ses.listVerifiedEmailAddresses(function(err, data) {
-        if(err) {
-            console.log(err);
-        } 
-        else {
-            verifiedEmails = data.VerifiedEmailAddresses;        } 
-    });
+var list = function () {
+  verifiedEmails = [];
+  ses.listVerifiedEmailAddresses(function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      verifiedEmails = data.VerifiedEmailAddresses;
+    }
+  });
 }
 
 list();
 
 var updateVideo = function (videoId, state) {
 
-  connection.query("UPDATE smarttools.videos SET state = '" + state + "' WHERE videoId = " + videoId , function(err, videos, fields) {
-      if (!err)
-        console.log('SUCCESS UPDATING VIDEO STATE'); 
-      else
-        console.log('ERROR UPDATING VIDEO STATE: ' + err);  
-    });  
+  connection.query("UPDATE smarttools.videos SET state = '" + state + "' WHERE videoId = " + videoId, function (err, videos, fields) {
+    if (!err)
+      console.log('SUCCESS UPDATING VIDEO STATE');
+    else
+      console.log('ERROR UPDATING VIDEO STATE: ' + err);
+  });
 }
 
 var convertVideo = function (video) {
-	console.log('--------------------------------------------------');
-	
-	var videoId = video.videoId;		
-	console.log('CONVERT video ID = ' + videoId);
-	ffmpeg('/Users/Diego/Documents/programs/smarttools/uploads/' + videoId)
-        .audioCodec('aac')
-        .videoCodec('libx264')
-        .size('320x200')
-        .on('error', function (err) {
-          console.log('ERROR CONVERTING VIDEO: ' + err.message);
-        })
-        .on('end', function (file) {
-          console.log('SUCCESS CONVERTING VIDEO');
-          updateVideo(videoId, "Converted");
-          if(_.contains(verifiedEmails, video.email)){	    
-          	sendMail(video.email, video.contestId);
-          } else {
-          	console.log('USER MUST VERIFY MAIL');
-			verify(video.email);
-          }
-        })
-        .save('/Users/Diego/Documents/programs/smarttools/convertedVideos/' + videoId + '.mp4');  
+  console.log('--------------------------------------------------');
+
+  var videoId = video.videoId;
+  console.log('CONVERT video ID = ' + videoId);
+  ffmpeg(__dirname + '/../efs/upload/' + videoId)
+    .audioCodec('aac')
+    .videoCodec('libx264')
+    .size('320x200')
+    .on('error', function (err) {
+      console.log('ERROR CONVERTING VIDEO: ' + err.message);
+    })
+    .on('end', function (file) {
+      console.log('SUCCESS CONVERTING VIDEO');
+      updateVideo(videoId, "Converted");
+      if (_.contains(verifiedEmails, video.email)) {
+        sendMail(video.email, video.contestId);
+      } else {
+        console.log('USER MUST VERIFY MAIL');
+        verify(video.email);
+      }
+    })
+    .save(__dirname + '/../efs/converted/' + videoId + '.mp4');
 };
 
 
 
-cron.schedule('* * * * *', function(){
-  	var date = new Date();
-  	console.log('\n' + date + ' SmartTools CRON is running now');  	  
+cron.schedule('* * * * *', function () {
+  var date = new Date();
+  console.log('\n' + date + ' SmartTools CRON is running now');
 
-    connection.query("SELECT * FROM smarttools.videos WHERE state = 'InProcess'", function(err, videos, fields) {
-      if (!err){
-        console.log('NUMBER OF VIDEOS: ' + videos.length);
-        _.each(videos,function(video){
-          updateVideo(video.videoId, "Process");
-        });
-        _.each(videos, function (video) {                    
-          convertVideo(video)
-        });
-      }        
-      else{
-        console.log('ERROR LOADING VIDEOS : ' + err);
-      }        
-    });
+  connection.query("SELECT * FROM smarttools.videos WHERE state = 'InProcess'", function (err, videos, fields) {
+    if (!err) {
+      console.log('NUMBER OF VIDEOS: ' + videos.length);
+      _.each(videos, function (video) {
+        updateVideo(video.videoId, "Process");
+      });
+      _.each(videos, function (video) {
+        convertVideo(video)
+      });
+    }
+    else {
+      console.log('ERROR LOADING VIDEOS : ' + err);
+    }
+  });
 
-	list();
+  list();
 
 });
 
