7a09d4379a2edfae7f238dcf237d9bfec08ee548
System Administrator <--global>
2016-11-01 00:02:35 -0500

    add last changes

--- sendmsg.js
+++ sendmsg.js
@@ -5,20 +5,20 @@ AWS.config.loadFromPath(__dirname + '/aws-config.json');
 var sqs = new AWS.SQS();
 
 var body = {
-  idVideo: '8076565045816',
+  idVideo: '2288757293999',
   email: 'diego@mensajerosurbanos.com'
 };
 var params = {
   MessageAttributes: {
     "video": {
       DataType: "String",
-      StringValue: "8076565045816"
+      StringValue: "2288757293999"
     }
   },
   MessageBody: JSON.stringify(body),
   QueueUrl: "https://sqs.us-west-2.amazonaws.com/942635221058/smarttools"
 };
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 1000; i++) {
   sqs.sendMessage(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else console.log(data);           // successful response
